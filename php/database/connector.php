36c1cec9cb2aa041e9e22d8bcd4e5ca725f8ac68
Waterfox <waterfox80@gmail.com>
2023-10-17 07:47:09 +0200

    Add first database info

---
+++ php/database/connector.php
@@ -0,0 +1,44 @@
+<?php
+
+$dbHost = '';
+$dbName = '';
+$dbUser = '';
+$dbPass = '';
+
+/**
+ * Initialize global variables with configuration file
+ */
+function initConfVar()
+{
+    global $dbHost, $dbName, $dbUser, $dbPass;
+
+    $envFilePath = '../../.env';
+
+    if (file_exists($envFilePath))
+    {
+        $envData = parse_ini_file($envFilePath);
+
+        $dbHost = $envData['DB_HOST'];
+        $dbName = $envData['DB_NAME'];
+        $dbUser = $envData['DB_USER'];
+        $dbPass = $envData['DB_PASS'];    
+    }
+    else
+    {
+        throw new Exception("[PC0] Essential `.env` file not found :: Critical | Path : ".$envFilePath);
+    }
+}
+
+/**
+ * Initialize a workable PDO
+ * @return PDO the PDO
+ * @see initConfVar()
+ */
+function initPDO()
+{
+    initConfVar();
+    global $dbHost, $dbName, $dbUser, $dbPass;
+    return new PDO("mysql:host=".$dbHost.";dbname=".$dbName."", $dbUser, $dbPass);
+}
+
+?>
