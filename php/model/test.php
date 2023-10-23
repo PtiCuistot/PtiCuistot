874cc13b2210d9e3ec2892d565b07592903a4f8b
Waterfox <waterfox80@gmail.com>
2023-10-23 16:13:44 +0200

    Update filename

--- php/model/test.php
+++ php/model/test.php
@@ -1,8 +1,8 @@
 <?php
 
 include_once("manager.php");
-include_once("category/categorymanager.php");
-include_once("category/category.php");
+include_once("recipe/recipe.php");
+include_once("recipeManager/recipemanager.php");
 
 $cat = new Category("Juice", "A juicy red fruit", 1);
 $catMan = new CategoryManager();
