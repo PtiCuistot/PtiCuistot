aa012a065e758f6c80876a53102216c989a632e0
Waterfox <waterfox80@gmail.com>
2023-10-24 09:00:23 +0200

    Add method to recipes

--- php/model/recipe/recipemanager.php
+++ php/model/recipe/recipemanager.php
@@ -3,6 +3,7 @@
 include_once("manager.php");
 include_once('recipe.php');
 
+
 class RecipeManager extends Manager
 {
     public function construct()
@@ -10,18 +11,42 @@ class RecipeManager extends Manager
         parent::__construct();
     }
 
-    public function getRecipeById(int $id): ?Recipe
+    public function getRecipes(int $limit=10)
     {
-        foreach($this->pdo->query("SELECT * FROM PC_RECIPE WHERE REP_ID = ".$id) as $row)
+
+        $recipes = [];
+
+        foreach($this->pdo->query("SELECT * FROM PC_RECIPE LIMIT".$limit) as $row)
         {
             $r = new Recipe(
+                $row['US_ID'],
                 $row['REP_TITLE'],
                 $row['REP_CONTENT'],
                 $row['REP_IMAGE'],
                 $row['REP_CREATED'],
                 $row['REP_UPDATED'],
                 $row['CAT_ID']);
-            $r->setId($row['US_ID']);
+            $r->setId($row['REP_ID']);
+
+            array_push($recipes, $r);
+        }
+
+        return $recipes;
+    }
+
+    public function getRecipeById(int $id)
+    {
+        foreach($this->pdo->query("SELECT * FROM PC_RECIPE WHERE REP_ID = ".$id) as $row)
+        {
+            $r = new Recipe(
+                $row['US_ID'],
+                $row['REP_TITLE'],
+                $row['REP_CONTENT'],
+                $row['REP_IMAGE'],
+                $row['REP_CREATED'],
+                $row['REP_UPDATED'],
+                $row['CAT_ID']);
+            $r->setId($row['REP_ID']);
             return $r;
         }
         return null;
@@ -29,33 +54,20 @@ class RecipeManager extends Manager
 
     public function insertRecipe(Recipe $recipe)
     {
-        $query = "INSERT INTO PC_RECIPE(US_ID, REP_TITLE, REP_CONTENT, REP_IMAGE, REP_CREATED, REP_UPDATED, REP_STATUT, CAT_ID) VALUES(2, ?, ?, ?, now(), now(), 1, ?)";
+        $query = "INSERT INTO PC_RECIPE(US_ID, REP_TITLE, REP_CONTENT, REP_IMAGE, REP_STATUT, CAT_ID) VALUES(?, ?, ?, ?, 1, ?)";
         $statement = $this->pdo->prepare($query);
         $statement->execute([
+            intval($recipe->getUserId()),
             $recipe->getTitle(),
             $recipe->getContent(),
             $recipe->getImage(),
-            $recipe->getCatId()
+            intval($recipe->getCatId())
         ]);
-        echo 'INSERT INTO PC_RECIPE(US_ID, REP_TITLE, REP_CONTENT, REP_IMAGE, REP_CREATED, REP_UPDATED, REP_STATUT, CAT_ID) VALUES(1, "';
-        echo $recipe->getTitle();
-        echo '", "';
-        echo $recipe->getContent();
-        echo '", "';
-        echo $recipe->getImage();
-        echo '", "';
-        echo $recipe->getCreated()->format("Y-m-d");
-        echo '", "';
-        echo $recipe->getUpdated()->format("Y-m-d");
-        echo '", ';
-        echo $recipe->getCatId();
-        echo ', 1)';
     }
 
     public function updateRecipe(Recipe $recipe)
     {
         $query = "UPDATE PC_RECIPE SET REP_ID = ?, REP_TITLE = ?, REP_IMAGE = ?, REP_CREATED = ?, REP_UPDATED = ?, REP_STATUS = ?, REP_CAT_ID = ? WHERE REP_ID = '".$recipe->getId()."'";
-        echo $query;
         $statement = $this->pdo->prepare($query);
         $statement->execute([
             $recipe->getTitle(),
@@ -66,6 +78,28 @@ class RecipeManager extends Manager
             $recipe->getStatut(),
             $recipe->getCatId()
         ]);
+    }
+
+    public function addTag(Recipe $recipe, Tag $tag)
+    {
+        $query = "INSERT INTO REFERENCED_BY (REP_ID, TA_ID, TA_CONTENT) VALUES (?, ?, ?)"; 
+        $statement = $this->pdo->prepare($query);
+        $statement->execute([
+            $recipe->getId(), 
+            $tag->getId(), 
+            $tag->getContent()
+        ]);
+    }
+
+    public function getTag(Recipe $recipe)
+    {
+        $tagArray = [];
+        $tagManager = new TagManager();
+        foreach($this->pdo->query("SELECT * FROM REFERENCED_BY WHERE REP_ID = ".$recipe->getId()) as $row)
+        {
+            array_push($tagArray, $tagManager->getTagById($row["TA_ID"]));
+        }   
+        return $tagArray;
     }
 }
 ?>
--- php/model/test.php
+++ php/model/test.php
@@ -1,27 +1,9 @@
 <?php
     include_once("../model/recipe/recipe.php");
-    include_once("../model/recipe/recipeManager.php");
-/*
-    $date = date("Y-m-d");
-    $recipe = new Recipe("a", "dd", "ff", $date, $date, 1);
+    include_once("../model/recipe/recipemanager.php");
 
     $rcp = new RecipeManager();
-    $rcp->insertRecipe($recipe);
+    $recipes = $rcp->getRecipes();
 
-    $rc = $rcp->getRecipeById(20);
-    echo $rc->getTitle();
-
-    */
-
-    include_once("../model/user/user.php");
-    include_once("../model/user/usermanager.php");
-
-    include_once("manager.php");
-
-    $mana = new Manager();
-    var_dump($mana);
-/*
-    $user = new User("Waterfox", "clement.baratin@etu.unicaen.fr", "ahudauyg", "a", "b");
-    $usermanager = new UserManager();
-    $usermanager->insertUser($user);*/
+    print_r($recipes)
 ?>
