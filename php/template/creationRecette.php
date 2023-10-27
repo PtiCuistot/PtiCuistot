bc7a592f62703f13b8e023ea075cf3ed6505eea6
cyprien <cyprien.delapoeze-dharambure@etu.unicaen.fr>
2023-10-27 10:09:07 +0200

    recipe list style

--- php/template/creationRecette.php
+++ php/template/creationRecette.php
@@ -7,14 +7,14 @@
     </head>
         <?php include('header.php');?>
         <div class="centerFormCreationRecette">
-            <form action="../treatment/treatment_recipe.php" id="recipeForm" method="POST">
+            <form action="../treatment/treatment_recipe.php" id="recipeForm" method="POST" class="needs-validation" novalidate>
                 <h1>Création de recettes</h1>
                 <h2>Informations de la recette</h2>
-                <div class="form-group has-warning has-feedback">
+                <div class="form-group">
                     <label class="control-label" for="recipeTitle">Nom de la recette :</label>
                     <input type="text" name="recipeTitle" class="form-control RecipeTitle" placeholder="Nom de la recette" id="recipeTitle" aria-describedby="inputWarning2Status" placeholder="Unité">
-                    <span class="glyphicon glyphicon-warning-sign form-control-feedback" aria-hidden="true"></span>
-                    <span id="recipeTitle" class="sr-only">(warning)</span>
+                    <div class="valid-tooltip">Looks good!</div>
+                    <div class="invalid-tooltip">Choose a name</div>
                 </div>
                 <div class="form-outline mb-4">
                     <label class="form-label" for="form6Example7">Additional information</label>
@@ -39,7 +39,7 @@
                             }
                         ?>
                     </select>
-                    <div class="invalid-feedback">Example invalid custom select feedback</div>
+                    <div class="valid-tooltip">Looks good!</div>
                 </div>
                 <h2>Ingrédients de la recette</h2>
                 <label for="ingredientsInput">Ajouter un ingrédient</label>
@@ -47,6 +47,7 @@
                     <div class="col">
                         <div class="form-group">
                             <select name="recipeIngredients" id="recipeIngredients" class="custom-select RecipeIngredients" required>
+                                <option value="">Sélectionner un ingrédient</option>
                                 <?php
                                     include_once("../model/manager.php");
                                     include_once("../model/ingredient/ingredient.php");
@@ -60,28 +61,30 @@
                                 <option>Créer un ingrédient</option>
                             </select>
                         </div>
-                        <div class="invalid-feedback">Example invalid custom select feedback</div>
+                        <div class="valid-tooltip">Looks good!</div>
                     </div>
                     <div class="col">
                         <div class="form-outline">
                             <input type="number" name="ingredientWeight" id="ingredientWeight"  class="form-control IngredientWeight" placeholder="Quantité"/>
                         </div>
                     </div>
+                    <div class="col">
+                        <div class="form-group has-warning has-feedback">
+                            <input type="text" id="ingredientName" placeholder="Nom de l'ingrédient" hidden>
+                            <input type="text" name="ingredientWeightUnity" class="form-control ingredientWeightUnity" id="ingredientWeightUnity" aria-describedby="inputWarning2Status" placeholder="Unité">
+                            <span class="glyphicon glyphicon-warning-sign form-control-feedback" aria-hidden="true"></span>
+                            <span id="ingredientWeightUnityStatus" class="sr-only">(warning)</span>
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group has-warning has-feedback">
-                    <input type="text" id="ingredientName" placeholder="Nom de l'ingrédient" hidden>
-                    <label class="control-label" for="ingredientWeightUnity">Unité :</label>
-                    <input type="text" name="ingredientWeightUnity" class="form-control ingredientWeightUnity" id="ingredientWeightUnity" aria-describedby="inputWarning2Status" placeholder="Unité">
-                    <span class="glyphicon glyphicon-warning-sign form-control-feedback" aria-hidden="true"></span>
-                    <span id="ingredientWeightUnityStatus" class="sr-only">(warning)</span>
-                </div>
+                
                 <div class="py-5 text-right"><a href="#" class="btn btn-dark px-5 py-3 text-uppercase" id="addIngredientButton">Ajouter un ingrédient</a></div>
                 <div>
                     <h4>Liste des ingrédients</h4>
                     <div id="ingredientList">
                     </div>
                 </div>
-                <input type="submit" value="Créer ma recette" id="submitButton" class="btn btn-outline-success btn-lg btn-block"></input>
+                <input type="submit" value="Créer ma recette" id="submitButton" class="btn btn-success btn-lg btn-block px-5 py-3"></input>
             </form>
         </div>
         <script src="../../assets/scripts/RecipeCreation.js"></script>
