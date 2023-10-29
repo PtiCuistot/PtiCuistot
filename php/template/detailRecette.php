68638a8751b1465ad05734c1770c8c7890ce7dbd
pti cha <cypr.jeux@gmail.com>
2023-10-29 16:21:32 +0100

    fix 404

--- php/template/detailRecette.php
+++ php/template/detailRecette.php
@@ -14,47 +14,43 @@ if (isset($_GET['id'])) {
     $um = new UserManager();
 
     $recipe = $rm->getRecipeById(intval($_GET['id']));
-    if($recipe != null){
+    if ($recipe != null) {
         if ($recipe->getValidate() == false) {
             if (isset($_SESSION['userId'])) {
                 if (intval($_SESSION['userId']) != intval($recipe->getUserId()) && !$_SESSION['admin']) {
                     $recipe = null;
-                    include('404.php');
                 }
             }
         }
-    }
-    else{
-        include('404.php');
+    } else {
     }
 }
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php if ($recipe != null) : ?>
 
-<head>
-    <meta charset="UTF-8">
-    <?php
-    if ($recipe != null) {
-        echo '<title>' . $recipe->getTitle() . '</title>';
-    } else {
-        echo '<title>Recette non trouvée</title>';
-    }
-    ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-    <?php include('link.php'); ?>
-</head>
-<?php include('header.php'); ?>
-<div class="Recipe">
-    <?php if ($recipe != null) : ?>
+    <head>
+        <meta charset="UTF-8">
+        <?php
+        if ($recipe != null) {
+            echo '<title>' . $recipe->getTitle() . '</title>';
+        }
+        ?>
+
+        <?php include('link.php'); ?>
+    </head>
+    <?php include('header.php'); ?>
+    <div class="Recipe">
         <img src="<?php echo $recipe->getImage(); ?>" height="650px" class="img-fluid rounded" alt="Image de la recette">
         <h1 class="display-3 RecipeTitle"><?php echo $recipe->getTitle(); ?></h1>
         <h3><i>Une recette de <?php echo $um->getUserById($recipe->getUserId())->getUsername(); ?></i></h3>
         <p>Dernière Mise à jour : <?php echo $recipe->getUpdated()->format('d/m/Y') ?> </p>
         <div class="container">
-    
+
             <h4>Liste des ingrédients</h4>
             <table class="table mb-4" id="ingredientArrayGlobal">
                 <thead>
@@ -66,13 +62,12 @@ if (isset($_GET['id'])) {
                 </thead>
                 <tbody id="ingredientArray">
                     <?php
-                    foreach($rm->getRecipeIngredients($recipe) as $row)
-                    {
+                    foreach ($rm->getRecipeIngredients($recipe) as $row) {
                         echo '
                         <tr>
-                            <td>'.$row['ing_name'].'</td>
-                            <td>'.$row['iw_weight'].'</td>
-                            <td>'.$row['iw_unity'].'</td>
+                            <td>' . $row['ing_name'] . '</td>
+                            <td>' . $row['iw_weight'] . '</td>
+                            <td>' . $row['iw_unity'] . '</td>
                         </tr>';
                     }
                     ?>
@@ -81,7 +76,7 @@ if (isset($_GET['id'])) {
 
             <h4>Recette</h4>
             <p class="RecipeContent"><?php echo $recipe->getContent(); ?></p>
-    
+
         </div>
 
 
@@ -97,10 +92,9 @@ if (isset($_GET['id'])) {
             <form action="updateRecipe">
                 <input class="btn btn-success" type="submit" value="Valider la recette">
             </form>
-        <?php endif; ?>
-    <?php else : ?>
-        <h1>Recette non trouvée</h1>
-    <?php endif; ?>
-</div>
-
-<?php include('footer.php'); ?>
+    </div>
+    <?php include('footer.php'); ?>
+<?php endif; ?>
+<?php else : ?>
+    <?php include('404.php'); ?>
+<?php endif; ?>
