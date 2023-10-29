e1eef44f51d64abadf17ec8c47db61d8dec72693
pti cha <cypr.jeux@gmail.com>
2023-10-29 15:05:06 +0100

    404

---
+++ php/template/404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>404</title>
+    <link rel="stylesheet" href="../../assets/styles/style.css">
+</head>
+
+<body>
+    <div class="glitch-wrapper">
+        <div class="glitch" data-text="Oups...Ce lien est incorrect :c">Oups...Ce lien est incorrect :c</div>
+    </div>
+</body>
+
+</html>
--- php/template/index.php
+++ php/template/index.php
@@ -1,52 +1,56 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Accueil</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 
-        <link rel="stylesheet" href="../../assets/styles/style.css">
-        <link rel="stylesheet" href="../../assets/styles/NavBar.css">
-    </head>
-        <?php include('header.php');?>
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <?php include('link.php'); ?>
+</head>
+<?php include('header.php'); ?>
 
-        <div class="Content">
-            <div>
-                
-            </div>
-            <div class="TwoRow">
-                <div class="DernieresRecettes">
-                    <h1>LES DERNIÈRES RECETTES</h1>
-                    <div>
-                        <img src="" alt="Image Recette">
-                        <p>AAA</p>
-                    </div>
-                    <div>
-                        <img src="" alt="Image Recette">
-                        <p>AAA</p>
-                    </div>
-                    <div>
-                        <img src="" alt="Image Recette">
-                        <p>AAA</p>
-                    </div>
+<div class="Content">
+    <div>
+
+    </div>
+    <div class="TwoRow">
+        <div class="DernieresRecettes">
+            <h1>LES DERNIÈRES RECETTES</h1>
+            <div class="scrollable-div">
+                <div class="inner-div">
+                    <img src="" alt="Image Recette">
+                    <p>Desc</p>
                 </div>
-                <div class="Edito">
-                    <div class="center">
-                        <img src="../../assets/images/Pticuisto.png" alt="PtiCuisto img">
-                    </div>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet congue neque non vulputate. Duis ultricies iaculis tincidunt. Vivamus a pellentesque sem. Nam tempus et metus in tempor. Nunc venenatis, risus fermentum sagittis vestibulum, eros nibh vehicula quam, in mattis augue lorem in magna. Nullam id nibh interdum, accumsan tellus sit amet, imperdiet turpis. Morbi ullamcorper dui ut ligula dignissim porttitor. Pellentesque quam libero, vehicula vitae fermentum eget, volutpat sit amet magna. Sed aliquet, purus non mattis semper, velit tortor dapibus mi, in tempor urna lacus sagittis ligula.
-                    </p>
+                <div class="inner-div">
+                    <img src="" alt="Image Recette">
+                    <p>Desc</p>
+                </div>
+                <div class="inner-div">
+                    <img src="" alt="Image Recette">
+                    <p>Desc</p>
+                </div>
+                <div class="inner-div">
+                    <img src="" alt="Image Recette">
+                    <p>Desc</p>
+                </div>
+                <div class="inner-div">
+                    <img src="" alt="Image Recette">
+                    <p>Desc</p>
+                </div>
+                <div class="inner-div">
+                    <img src="" alt="Image Recette">
+                    <p>Desc</p>
                 </div>
             </div>
         </div>
-
-        <?php include('footer.php');?>
+        <div class="Edito">
+            <div class="center">
+                <img src="../../assets/images/Pticuisto.png" alt="PtiCuisto img">
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet congue neque non vulputate. Duis ultricies iaculis tincidunt. Vivamus a pellentesque sem. Nam tempus et metus in tempor. Nunc venenatis, risus fermentum sagittis vestibulum, eros nibh vehicula quam, in mattis augue lorem in magna. Nullam id nibh interdum, accumsan tellus sit amet, imperdiet turpis. Morbi ullamcorper dui ut ligula dignissim porttitor. Pellentesque quam libero, vehicula vitae fermentum eget, volutpat sit amet magna. Sed aliquet, purus non mattis semper, velit tortor dapibus mi, in tempor urna lacus sagittis ligula.
+            </p>
+        </div>
+    </div>
+</div>
+<?php include('footer.php'); ?>
