83fee53ed7a88af7a536872cd8d71e536b6f0773
pti cha <cypr.jeux@gmail.com>
2023-10-29 15:45:06 +0100

    404 button

--- php/template/404.php
+++ php/template/404.php
@@ -10,6 +10,12 @@
 <body>
     <div class="glitch-wrapper">
         <div class="glitch" data-text="Oups...Ce lien est incorrect :c">Oups...Ce lien est incorrect :c</div>
+        <a type="button" class="btn btn-info Button404" href="index.php">Retour à l'acceuil</a>
+        <div class="citation">
+            <p class="italic">"On apprend la cuisine avec celle des autres...</p>
+            <p class="italic">...A un moment donnée, on fait la sienne"</p>
+            <p>Jean Francois Piège</p>
+        </div>
     </div>
 </body>
 
