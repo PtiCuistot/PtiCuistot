50b936ea596a3ff4d59278016a4a1b04d383c9d7
SirPunisher <andgel.brissaud@etu.unicaen.fr>
2023-10-30 12:21:29 +0100

    add 3 popups for filters

--- assets/scripts/popup.js
+++ assets/scripts/popup.js
@@ -1,8 +1,29 @@
-const modalContainer = document.querySelector(".modalContainer");
-const modalTrigger = document.querySelectorAll(".modalTrigger");
+// modal category
+const modalContainerCategory = document.querySelector(".modalContainerCategory");
+const modalTriggerCategory = document.querySelectorAll(".triggerCategory");
 
-modalTrigger.forEach( trigger => trigger.addEventListener("click", toggleModal));
+modalTriggerCategory.forEach( triggerCategory => triggerCategory.addEventListener("click", toggleModalCategory));
 
-function toggleModal(){
-    modalContainer.classList.toggle("active");
+function toggleModalCategory(){
+    modalContainerCategory.classList.toggle("active");
+}
+
+// modal title
+const modalContainerTitle = document.querySelector(".modalContainerTitle");
+const modalTriggerTtile = document.querySelectorAll(".triggerTitle");
+
+modalTriggerTtile.forEach( triggerTitle => triggerTitle.addEventListener("click", toggleModalTitle));
+
+function toggleModalTitle(){
+    modalContainerTitle.classList.toggle("active");
+}
+
+// modal ingredient
+const modalContainerIngredient = document.querySelector(".modalContainerIngredient");
+const modalTriggerIngredient = document.querySelectorAll(".triggerIngredient");
+
+modalTriggerIngredient.forEach( triggerIngredient => triggerIngredient.addEventListener("click", toggleModalIngredient));
+
+function toggleModalIngredient(){
+    modalContainerIngredient.classList.toggle("active");
 }
--- php/template/popup.php
+++ php/template/popup.php
@@ -1,10 +1,29 @@
-<div class="modalContainer">
-    <div class="ov modalTrigger"></div>
+<!-- modal category -->
+<div class="modalContainerCategory">
+    <div class="ov triggerCategory"></div>
     <div class="popup" role="dialog" aria-describedby="dialogDesc" aria-labelledby="modalTitle" >
-        <button aria-label="close modal" class="close-modal modalTrigger">X</button>
-        <h1 id="modalTitle">Contenue</h1>
-        <p id="dialogDesc">lorem ipsum dqscsc dsq q q qf </p>
+        <button aria-label="close modal" class="close-modal triggerCategory">X</button>
+        <h1 id="modalTitle">Filtrer par categorie</h1>
+        <p id="dialogDesc">Selectionnez votre filtre</p>
     </div>
 </div>
 
-<button class="modal-btn modalTrigger">open</button>
+<!-- modal title -->
+<div class="modalContainerTitle">
+    <div class="ov triggerTitle"></div>
+    <div class="popup" role="dialog" aria-describedby="dialogDesc" aria-labelledby="modalTitle" >
+        <button aria-label="close modal" class="close-modal triggerTitle">X</button>
+        <h1 id="modalTitle">Filtrer par titre</h1>
+        <p id="dialogDesc">Selectionnez votre filtre</p>
+    </div>
+</div>
+
+<!-- modal ingredient -->
+<div class="modalContainerIngredient">
+    <div class="ov triggerIngredient"></div>
+    <div class="popup" role="dialog" aria-describedby="dialogDesc" aria-labelledby="modalTitle" >
+        <button aria-label="close modal" class="close-modal triggerIngredient">X</button>
+        <h1 id="modalTitle">Filtrer par ingrédient</h1>
+        <p id="dialogDesc">Selectionnez votre filtre</p>
+    </div>
+</div>
