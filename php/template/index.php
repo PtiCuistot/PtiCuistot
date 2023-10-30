637f7313f409547bff032a7b454e8fe6669341bf
pti cha <cypr.jeux@gmail.com>
2023-10-30 19:10:45 +0100

    link recipeCreation and modif tags

--- php/template/index.php
+++ php/template/index.php
@@ -47,7 +47,7 @@
         </div>
         <div class="Edito">
             <div class="center">
-                <img src="../../assets/images/Pticuisto.png" alt="PtiCuisto img">
+                <img src="assets/images/Pticuisto.png" alt="PtiCuisto img">
             </div>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet congue neque non vulputate. Duis ultricies iaculis tincidunt. Vivamus a pellentesque sem. Nam tempus et metus in tempor. Nunc venenatis, risus fermentum sagittis vestibulum, eros nibh vehicula quam, in mattis augue lorem in magna. Nullam id nibh interdum, accumsan tellus sit amet, imperdiet turpis. Morbi ullamcorper dui ut ligula dignissim porttitor. Pellentesque quam libero, vehicula vitae fermentum eget, volutpat sit amet magna. Sed aliquet, purus non mattis semper, velit tortor dapibus mi, in tempor urna lacus sagittis ligula.
