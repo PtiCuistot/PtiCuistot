5f7501f02ec70345b7d412f62a0dbd98d366fdb4
Waterfox <waterfox80@gmail.com>
2023-11-07 08:44:58 +0100

    Update disconnect

--- php/treatment/disconnect.php
+++ php/treatment/disconnect.php
@@ -1,10 +1,8 @@
 <?php
 
-if (!(session_status() === PHP_SESSION_NONE))
-{
-    unset($_SERVER['userId']);
-    unset($_SESSION['admin']); 
-    header('Location: ../../index.php');
-}
+unset($_SERVER['userId']);
+unset($_SESSION['admin']); 
+header('Location: ../../index.php');
+
 
 ?>
